8ff7eb1f63aa3bf0c669ba063e2dca9ed81e8b03
Gakkou <lucas.soares.avila@gmail.com>
2016-09-29 05:41:14 -0300

    ESSA PORRA TA FODA PARA CARALHOOOOOOgit add placar.txt joguinhoagoravai.py git add placar.txt joguinhoagoravai.py !

--- joguinhoagoravai.py
+++ joguinhoagoravai.py
@@ -1,4 +1,4 @@
- #coding=UTF-8
+#coding=UTF-8
 '''
 	Joguinho:
 	Lucas S. Avila 111136
@@ -124,35 +124,36 @@ def contador(nivel, wingame, resolu1, resolu2):
 	
 # Criando inimigos e suas características únicas
 def criarini(nivel, wingame, resolu1, resolu2):
-	# Criar o desenho das imagens!!!!
-	
-	# Respawn
-	iniimg = ["fabiob1.png", "dirceu1.png", "maranhao1.png", "renanc1.png", "aecio.png", "feliciano1.png", "cunha1.png", "bolsonaropai2.png", "temer1.png"]
-	mexer = [0, 0, 0, 0, 0, nivel, random.randint(nivel, 2*nivel), random.randint(nivel, 3*nivel), random.randint(nivel, 4*nivel)]
-	vida = [random.randint(1, 2+nivel/4), random.randint(1+nivel/4, 2+nivel/2), random.randint(1+nivel/2, 2+nivel), random.randint(nivel, 4*nivel/3), random.randint(4*nivel/3, 3*nivel/2), random.randint(1, 2+nivel/4), random.randint(nivel/4, 2+nivel/2), random.randint(nivel/2, 2+nivel), random.randint(nivel, 4*nivel/3)]
+	# Características de cada
+	iniimg = ["fabiob2.png", "dirceu1.png", "maranhao2.png", "renanc2.png", "aecio.png", "feliciano2.png", "cunha1.png", "bolsonaropai2.png", "temer1.png"]
+	mexer = [0, 0, 0, 0, 0, nivel, random.randint(1+(2*nivel)/3, 2+nivel), random.randint(nivel, 1+(3*nivel)/2), random.randint(nivel, 1+(4*nivel)/3)]
+	vida = [random.randint(1, 2+nivel/4), random.randint(1+nivel/4, 2+nivel/2), random.randint(1+nivel/2, 2+nivel), random.randint(nivel, 4*nivel/3), random.randint(4*nivel/3, 3*nivel/2), random.randint(1, 2+nivel/4), random.randint(1+nivel/4, 2+nivel/2), random.randint(1+nivel/2, 2+nivel), random.randint(nivel, 4*nivel/3)]
 	# Desenhando no jogo
 	global nini
 	global initela
 	global imginitela
 	global vidainitela
 	global mexerinitela
-	initela = []
-	imginitela = []
-	vidainitela = []
-	mexerinitela = []
+	global celas
+	initela = []		# Área de detecção
+	imginitela = []		# A imagem
+	vidainitela = []	# A vida
+	mexerinitela = []	# A movimentação
+	celas = []
 	nini = random.randint(1+nivel/4, 4+nivel/4)
 	cont = 0
 	while(cont < nini):
 		pontoa = Point(random.randint(0, resolu1-200), random.randint(0, resolu2-200))
 		pontob = Point(pontoa.getX()+200, pontoa.getY()+200)
 		initela.append(Rectangle(pontoa, pontob))
-		initela[cont].draw(wingame)		# Temporário
+		#initela[cont].draw(wingame)		# Temporário
 		img = random.choice(iniimg)
 		imginitela.append(Image(initela[cont].getCenter(), img))
 		imginitela[cont].draw(wingame)
 		cont1 = iniimg.index(img)
 		vidainitela.append(vida[cont1])
 		mexerinitela.append(mexer[cont1])
+		celas.append(Image(initela[cont].getCenter(), "cela.png"))
 		cont += 1
 
 # Rodando o jogo
@@ -160,8 +161,10 @@ def Jogo():
 	resolu_config()						# Chama a janela de configuração
 	wingame = win_principal(resolu1, resolu2)		# Cria a Janela Principal
 	instru(wingame, resolu1, resolu2)			# Mostra as instruções
+	global score
 	nivel = 1
-	vida = 3		#vida do jogador
+	score = 0		# Pontuação
+	vida = 3		# Vida do jogador
 	while(vida > 0):
 		ninitela = 1
 		tempo = 0.0
@@ -177,10 +180,11 @@ def Jogo():
 					pontob = initela[cont].getP2()
 					if(click.getX() >= pontoa.getX() and click.getX() <= pontob.getX() and click.getY() >= pontoa.getY() and click.getY() <= pontob.getY()):
 						if(vidainitela[cont] != 0):
+							score += 100
 							vidainitela[cont] += -1
 							if(vidainitela[cont] == 0):
-								initela[cont].undraw() #temporário
-								imginitela[cont].undraw()
+								#initela[cont].undraw() #temporário
+								celas[cont].draw(wingame)
 								ninitela += -1
 							cont = -1
 						else:
@@ -191,16 +195,91 @@ def Jogo():
 					cont = -1
 			cont = 0
 			while(cont < nini):
-				moverx = mexerinitela[cont]*random.randint(-1,1)
-				movery = mexerinitela[cont]*random.randint(-1,1)
-				initela[cont].move(moverx, movery)
-				imginitela[cont].move(moverx,movery)
+				if(vidainitela[cont] > 0):
+					moverx = mexerinitela[cont]*random.randint(-1,1)
+					movery = mexerinitela[cont]*random.randint(-1,1)
+					initela[cont].move(moverx, movery)
+					imginitela[cont].move(moverx, movery)
+					celas[cont].move(moverx, movery)
 				cont += 1
 			time.sleep(0.01)
 			tempo += 0.01
-		if(ninitela != 0):
-			#diminuir vida com número de inimigos não mortos
-			vida += -1
+		cont = 0
+		while(cont < nini):
+			if(vidainitela[cont] > 0):
+				#initela[cont].undraw()
+				imginitela[cont].undraw()
+				vida += -1
+			else:
+				imginitela[cont].undraw()
+				celas[cont].undraw()
+			cont += 1
 		nivel += 1
+		score +=10*(9.0 - nivel/4 - tempo) + 1000
+	wingame.close()
+	
+# Tela de parabéns
+def parabains():
+	wincongrats = GraphWin("Parabéns", 300, 200)
+	texto = Text(Point(150, 100), "Primeiramente FORA TEMER!!!\n\nSua pontuação foi de " + str(score) + " pontos.")
+	texto.draw(wincongrats)
+	wincongrats.getMouse()
+	wincongrats.close()
+
+# Tela de score
+def telascore():
+	winscore = GraphWin("Placar", 500, 600)
+	entrada = Entry(Point(250, 300), 10)
+	entrada.setText("Usuario")
+	entrada.draw(winscore)
+	texto = Text(Point(250, 200), "Digite a o seu nome:")
+	texto.draw(winscore)
+	botaop1 = Rectangle(Point(225, 550), Point(275, 575))
+	botaop1.setFill("White")
+	botaop1.draw(winscore)
+	botaop2 = Text(botaop1.getCenter(), "Ok!")
+	botaop2.draw(winscore)
+	click = winscore.getMouse()
+	while(click.getX() < botaop1.getP1().getX() or click.getX() > botaop1.getP2().getX() or click.getY() < botaop1.getP1().getY() or click.getY() > botaop1.getP2().getY()):
+		click = winscore.getMouse()
+	nomeplayer = entrada.getText()
+	entrada.undraw()
+	texto.undraw()
+	botaop1.undraw()
+	botaop2.undraw()
+	arqplacar = open("placar.txt", 'r')
+	texto = arqplacar.readlines()
+	verif = 0
+	for elem in texto:
+		cont = -2
+		scoret = ''
+		while(cont != 0):
+			if(elem[cont] == ' '):
+				cont = 0
+			else:
+				scoret = elem[cont] + scoret
+				cont+= -1 
+		scoret = eval(scoret)*1.0
+		if(score > scoret):
+			lugar = texto.index(elem)
+			break
+	cont1 = len(texto)
+	ultimalinha = texto[-1]
+	texto.append(ultimalinha)
+	while(cont1 > lugar):
+		texto[cont1-1] = texto[cont1-2]
+		cont1 += -1
+	texto[lugar] = nomeplayer + " -- " + str(score) + "\n"
+	arqplacar = open("placar.txt", 'w')
+	arqplacar.writelines(texto)
+	arqplacar.close()
+	arqplacar = open("placar.txt", 'r')
+	texto = arqplacar.read()
+	arqplacar.close()
+	textotela = Text(Point(250, 300), texto)
+	textotela.draw(winscore)
+	winscore.getMouse()
 
 Jogo()
+parabains()
+telascore()
